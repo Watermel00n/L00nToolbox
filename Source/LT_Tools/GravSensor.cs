f03d528076031f5ae6c6c4da8b966ff9ff6eef9b
Watermel00n <95552493+Watermel00n@users.noreply.github.com>
2023-07-08 21:37:15 +0800

    Add files via upload

---
+++ Source/LT_Tools/GravSensor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace LT_Tools
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class LT_GravSensor : PartModule
+    {
+        public ModuleGenerator converter;
+        public override void OnAwake()
+        {
+            if (part != null)
+            {
+                converter = part.FindModuleImplementing<ModuleGenerator>();
+            }
+        }
+        public void FixedUpdate()
+        {
+            if (!HighLogic.LoadedSceneIsFlight) 
+            {
+                return;
+            }
+            
+            else if (vessel.Landed == false && converter != null)
+            {
+                if (converter.generatorIsActive == true)
+                { 
+                    converter.Shutdown();
+                    ScreenMessages.PostScreenMessage("Vessel must be landed for converter to start! This converter will no longer work!", 5.0f);
+                }
+            }
+        }
+    }
+}
---
+++ Source/LT_Tools/Hello.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LT_Tools
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class Hello : MonoBehaviour
+    {
+        public void Start()
+        {
+            UnityEngine.Debug.Log("LT_Tools loaded!");
+        }
+    }
+}
