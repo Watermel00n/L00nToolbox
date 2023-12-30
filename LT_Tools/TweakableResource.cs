99bcaea69845d3133935ceecfb757b7c86729f85
Watermel00n <95552493+Watermel00n@users.noreply.github.com>
2023-12-30 21:41:49 +0800

    Add files via upload

---
+++ LT_Tools/TweakableResource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LT_Tools
+{
+    public class LT_UnTweakableResource:PartModule
+    {
+        [KSPField]
+        public string resourceName = "ScienceResource";
+        public void FixedUpdate()
+        {
+            if (part.Resources.Contains(resourceName))
+            {
+                part.Resources[resourceName].isTweakable = false;
+            }
+            else
+                ScreenMessages.PostScreenMessage("Part doesn't have any of the resource specified in the config!");
+        }
+        public override string GetInfo()
+        {
+            var outputstring = string.Empty;
+            outputstring = "Untweakable Resource:" + resourceName;
+            return outputstring;
+        }
+    }
+}
